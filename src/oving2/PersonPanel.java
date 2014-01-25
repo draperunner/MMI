7f7cfd532b6acc72da5cea979348348958534528
Mats Byrkjeland <matsbyr@gmail.com>
2014-01-25 20:42:46 +0100

    Øving 2
    Ferdig

--- src/oving2/PersonPanel.java
+++ src/oving2/PersonPanel.java
@@ -2,76 +2,89 @@ package oving2;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import oving1.ButtonsNText;
 
-public class PersonPanel extends JPanel {
+public class PersonPanel extends JPanel implements ActionListener, ChangeListener {
 
 	private Person model;
-
-	private GridBagLayout layout;
-	private JLabel nameLabel, emailLabel, dateOfBirthLabel, genderLabel,
-			heightLabel;
+	private JLabel nameLabel, emailLabel, dateOfBirthLabel, genderLabel, heightLabel;
 	private JTextField nameTextField, emailTextField, dateOfBirthTextField;
 	private JComboBox gender;
 	private JSlider height;
 
 	public PersonPanel() {
 
-		JLabel nameLabel = new JLabel("Name:");
+		GridBagConstraints c;
+		setLayout(new GridBagLayout());
+		c = new GridBagConstraints();
+		
+		c.gridx = 0;
+		c.gridy = 0;
+		nameLabel = new JLabel("Name:");
+		add(nameLabel, c);
+
+		c.gridx = 1;
 		nameTextField = new JTextField(10);
 		nameTextField.setName("NamePropertyComponent");
+		nameTextField.addActionListener(this);
+		add(nameTextField, c);
 
-		JLabel emailLabel = new JLabel("Email:");
+		c.gridx = 0;
+		c.gridy = 1;
+		emailLabel = new JLabel("Email:");
+		add(emailLabel, c);
+		
+		c.gridx = 1;
 		emailTextField = new JTextField(10);
 		emailTextField.setName("EmailPropertyComponent");
+		emailTextField.addActionListener(this);
+		add(emailTextField, c);
 
-		JLabel dateOfBirthLabel = new JLabel("Date of Birth:");
+		c.gridx = 0;
+		c.gridy = 2;
+		dateOfBirthLabel = new JLabel("Date of Birth:");
+		add(dateOfBirthLabel, c);
+		
+		c.gridx = 1;
 		dateOfBirthTextField = new JTextField(10);
 		dateOfBirthTextField.setName("DateOfBirthPropertyComponent");
+		dateOfBirthTextField.addActionListener(this);
+		add(dateOfBirthTextField, c);
 
+		c.gridx = 0;
+		c.gridy = 3;
 		JLabel genderLabel = new JLabel("Gender:");
+		add(genderLabel, c);
+		
+		c.gridx = 1;
 		gender = new JComboBox();
 		gender.setName("GenderPropertyComponent");
 		gender.addItem(Gender.male);
 		gender.addItem(Gender.female);
+		gender.addActionListener(this);
+		add(gender, c);
 
-		JLabel heightLabel = new JLabel("Height:");
+		c.gridx = 0;
+		c.gridy = 4;
+		heightLabel = new JLabel("Height:");
+		add(heightLabel, c);
+		
+		c.gridx = 1;
 		height = new JSlider(120, 220);
 		height.setName("HeightPropertyComponent");
-
-		layout = new GridBagLayout();
-		layout.addLayoutComponent("", nameLabel);
-		layout.addLayoutComponent("Name", nameTextField);
-		layout.addLayoutComponent("Email", emailTextField);
-		layout.addLayoutComponent("Date of Birth", dateOfBirthTextField);
-		layout.addLayoutComponent("Gender", gender);
-		layout.addLayoutComponent("Height", height);
-
-		add(nameLabel);
-		add(nameTextField);
-		add(emailLabel);
-		add(emailTextField);
-		add(dateOfBirthLabel);
-		add(dateOfBirthTextField);
-		add(genderLabel);
-		add(gender);
-		add(heightLabel);
-		add(height);
-
-		Person per = new Person("Per");
-		per.setEmail("per@byrkjeland.net");
-		per.setDateOfBirth("030685");
-		per.setGender(Gender.male);
-		per.setHeight(183);
-		setModel(per);
+		height.addChangeListener(this);
+		add(height, c);
 	}
 
 	public Person getModel() {
@@ -80,10 +93,6 @@ public class PersonPanel extends JPanel {
 
 	public void setModel(Person model) {
 		this.model = model;
-		update();
-	}
-
-	private void update() {
 		nameTextField.setText(model.getName());
 		emailTextField.setText(model.getEmail());
 		dateOfBirthTextField.setText(model.getDateOfBirth());
@@ -91,14 +100,38 @@ public class PersonPanel extends JPanel {
 		height.setValue(model.getHeight());
 	}
 
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == nameTextField) {
+			model.setName(nameTextField.getText());
+		}
+		else if (e.getSource() == emailTextField) {
+			model.setEmail(emailTextField.getText());
+		}
+		else if (e.getSource() == dateOfBirthTextField) {
+			model.setDateOfBirth(dateOfBirthTextField.getText());
+		}
+		else if (e.getSource() == gender) {
+			model.setGender((Gender) gender.getSelectedItem());
+		}
+		else if (e.getSource() == height) {
+			model.setHeight(height.getValue());
+		}	
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		model.setHeight(height.getValue());		
+	}
+
+
 	public static void main(String[] args) {
 		PersonPanel pp = new PersonPanel();
-		pp.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
 		JFrame frame = new JFrame();
 		frame.setContentPane(pp);
 		frame.pack();
 		frame.setVisible(true);
-	}
 
-}
+	}
+}	
